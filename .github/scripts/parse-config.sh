64a6119d85f6efc1ac0bb63a60e88bbca1fa0411
Alec Dorrington <alecdorrington@gmail.com>
2026-03-20 15:53:56 +0100

    Fixed default TARGET_SYNC_BRANCH to avoid naming collisions.

--- .github/scripts/parse-config.sh
+++ .github/scripts/parse-config.sh
@@ -51,7 +51,7 @@ CHILDREN=$(jq -c --rawfile default_pr_body .github/templates/pull-request-body.m
       ("https://github.com/" + $repository) as $url |
       .target.repository = $repository |
       .target.branch //= "" |
-      .target.syncBranch //= "sync/" + .source.repository |
+      .target.syncBranch //= "sync:" + .source.repository + "//" + .source.branch + "//" + .source.root |
       .target.root //= "." |
       .target.url = $url
     ) |
