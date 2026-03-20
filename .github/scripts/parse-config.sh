df1fa17012079014b43d086f02d3ddc6f825e0e9
Alec Dorrington <alecdorrington@gmail.com>
2026-03-20 16:17:13 +0100

    Improved branch name.

--- .github/scripts/parse-config.sh
+++ .github/scripts/parse-config.sh
@@ -52,7 +52,7 @@ CHILDREN=$(jq -c --rawfile default_pr_body .github/templates/pull-request-body.m
       .target.repository = $repository |
       .target.branch //= "" |
       .target.syncBranch //= "sync#" + .source.repository + "_" + .source.branch + "_" + .source.root +
-        "->" + .target.root + ";" |
+        "->" + (.target.root // ".") + ";" |
       .target.root //= "." |
       .target.url = $url
     ) |
